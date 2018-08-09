d1cc7473ce04e0eeafc8eefa7709bff23e16fca0
kobe <kobe@192.168.1.107>
2018-08-09 08:29:28 +0800

    add config

--- config-service/src/main/java/com/hsbc/ConfigServiceApplication.java
+++ config-service/src/main/java/com/hsbc/ConfigServiceApplication.java
@@ -3,9 +3,11 @@ package com.hsbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer//if don't add this annotation, config-client project can't load github properties
+@EnableEurekaClient//define it is Eureka client
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
